7ceba330b2aa129a7c35f1bb4143dfb8bc75fd93
eryson <eryson.ktm@gmail.com>
2020-06-08 16:38:07 -0300

    Melhorias e tratamentos de erros

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ const app = express();
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
+const morgan = require('morgan');
 
+app.use(morgan('dev'));
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
+
+// QUANDO NÃO ENCONTRA ROTAS
+app.use((req, res, next) => {
+    const erro = new Error('Não Encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
 
 module.exports = app;
