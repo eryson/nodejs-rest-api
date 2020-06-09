5892a0e9951095d7c5cbafe750eea94768993999
eryson <eryson.ktm@gmail.com>
2020-06-09 11:21:50 -0300

    Conexao da API com Banco de Dados

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,47 +1,114 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 // RETORNA TODOS OS PRODUTOS
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'GET: Retorna os Produtos'
+    mysql.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error})};
+        conn.query(
+            'SELECT * FROM produtos',
+            (error, resultado, field) => {
+                conn.release();
+
+                if (error) {return res.status(500).send({error: error, response: null})};
+
+                return res.status(200).send({
+                    response: resultado
+                })
+            }
+        )
     });
 });
 
 // INSERE UM PRODUTO
 router.post('/', (req, res, next) => {
-    
-    const produto = {
-        nome: req.body.nome,
-        preco: req.body.preco
-    };
+    mysql.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error})};
+        conn.query(
+            'INSERT INTO produtos (nome, preco) VALUES (?,?)',
+            [req.body.nome, req.body.preco],
+            (error, resultado, field) => {
+                conn.release();
 
-    res.status(200).send({
-        mensagem: 'POST: Produto Foi Criado',
-        produtoCriado: produto
+                if (error) {return res.status(500).send({error: error, response: null})};
+
+                res.status(201).send({
+                    mensagem: 'Produto Inserido com Sucesso',
+                    id_produto: resultado.insertId
+                })
+            }
+        )
     });
 });
 
 // RETORNA OS DADOS DE UM PRODUTO
 router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto;
-    res.status(200).send({
-        mensagem: 'GET: Detalhes do Produto',
-        id_produto: id
+    mysql.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error})};
+        conn.query(
+            'SELECT * FROM produtos WHERE id_produtos = ?;',
+            [req.params.id_produto],
+            (error, resultado, field) => {
+                conn.release();
+
+                if (error) {return res.status(500).send({error: error, response: null})};
+
+                return res.status(200).send({
+                    response: resultado
+                })
+            }
+        )
     });
 });
 
 // ALTERA UM PRODUTO
 router.patch('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'PATCH: Produto Alterado'
+    mysql.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error})};
+        conn.query(
+            `UPDATE produtos
+                SET nome       = ?,
+                    preco      = ?
+              WHERE id_produtos = ?`,
+            
+            [
+                req.body.nome, 
+                req.body.preco,
+                req.body.id_produto
+            ],
+
+            (error, resultado, field) => {
+                conn.release();
+
+                if (error) {return res.status(500).send({error: error, response: null})};
+
+                res.status(202).send({
+                    mensagem: 'Produto Alterado com Sucesso'
+                })
+            }
+        )
     });
 });
 
 // EXCLUI UM PRODUTO
 router.delete('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'DELETE: Produto Excluido'
+    mysql.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error})};
+        conn.query(
+            'DELETE FROM produtos WHERE id_produtos = ?',
+            [req.body.id_produto],
+
+            (error, resultado, field) => {
+                conn.release();
+
+                if (error) {return res.status(500).send({error: error, response: null})};
+
+                res.status(202).send({
+                    mensagem: 'Produto Excluido com Sucesso'
+                })
+            }
+        )
     });
 });
 
