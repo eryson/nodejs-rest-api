8a8beaf7b7a8db29a23e3f095414894c40780bab
eryson <eryson.ktm@gmail.com>
2020-06-10 17:30:25 -0300

    Protegendo as rotas com jsonwebtoken

--- routes/produtos.js
+++ routes/produtos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const multer = require('multer');
+const login = require('../middleware/login');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -62,7 +63,7 @@ router.get('/', (req, res, next) => {
 });
 
 // INSERE UM PRODUTO
-router.post('/', upload.single('produto_imagem'), (req, res, next) => {
+router.post('/', login.obrigatorio, upload.single('produto_imagem'), (req, res, next) => {
     console.log(req.file);
     mysql.getConnection((error, conn) => {
         if (error) {return res.status(500).send({error: error})};
@@ -138,7 +139,7 @@ router.get('/:id_produto', (req, res, next) => {
 });
 
 // ALTERA UM PRODUTO
-router.patch('/', (req, res, next) => {
+router.patch('/', login.obrigatorio, (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) {return res.status(500).send({error: error})};
         conn.query(
@@ -182,7 +183,7 @@ router.patch('/', (req, res, next) => {
 });
 
 // EXCLUI UM PRODUTO
-router.delete('/', (req, res, next) => {
+router.delete('/', login.obrigatorio, (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) {return res.status(500).send({error: error})};
         conn.query(
